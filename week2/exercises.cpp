1a9c4f9316fa81ab6acdab4f497058a9591646f1
nick rasmussen <nickras10@gmail.com>
2025-02-27 15:56:10 +0100

    last week exervisre

--- week2/exercises.cpp
+++ week2/exercises.cpp
@@ -2,39 +2,24 @@
 #include <vector>
 #include <unistd.h>
 
-int main(){
-    std::vector<int> A = {31, 41, 59, 26, 41, 58};
-    int n = 6;
+int main(){ //INSERTION SORT
+    int A[] = {31, 41, 59, 26, 41, 58};
+    int n = sizeof(A) / sizeof(A[0]);
     int key, j;
     for(int i = 1; i < n; i++){
         key = A[i];
         j = i - 1;
-        while j >= 0 && A[j] < key{
+        while (j >= 0 && A[j] > key) {
+            int tmp = A[j + 1];
             A[j + 1] = A[j];
+            A[j] = tmp;
             j = j - 1;
         }
-        A[j + 1] = key;
+    }
+    printf("Sorted array is:\n");
+    for (int i = 0; i < n; i++) {
+        std::cout << A[i] << " ";
     }
 
     return 0;
 }
-// #include <iostream>
-// #include <vector>
-// #include <unistd.h>
-
-// int main(){
-//     std::vector<int> A = {31, 41, 59, 26, 41, 58};
-//     int n = 6;
-//     int key, j;
-//     for(int i = 1; i < n; i++){
-//         key = A[i];
-//         j = i - 1;
-//         while j >= 0 && A[j] < key{
-//             A[j + 1] = A[j];
-//             j = j - 1;
-//         }
-//         A[j + 1] = key;
-//     }
-
-//     return 0;
-// }
