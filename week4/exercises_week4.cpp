52a96e615a9b6bab08e1b983d6498cd2912a18fb
nick rasmussen <nickras10@gmail.com>
2025-03-06 17:50:51 +0100

    week4

---
+++ week4/exercises_week4.cpp
@@ -0,0 +1,18 @@
+//exervise 2: answer is C D U T N R M
+//exervise 3.1 + 3.2: FOO --> c= 4. Den går videre til næste link i listen og adder +=1 til C hver gang, indtil C er null
+//exercise 3.3 + 3.4: 95 da den iterater gennem link list og lægger alle keys sammen
+//exercise 4.1: enten fejl eller 2 frem
+//exercise 4.2: easy
+//exercise 4.3: t.next peger på t
+//exercise 5.1: a.next = c OG c.prev = a
+//exervise 5.2: samme som ovenfor bare med 2 elementer ved siden af hinanden. 
+//Det er const time fordi det er bare lineær iteration fremad
+//exercise 7.1: iterate fremad, og hvis new_in < curr_elem, så indsæt før den
+/*
+7.2: hvis alle pointers er i en array med indexes så ja.
+8.0 Hvis det er single link, så bare start far head, og gør den til tail
+
+
+lecture 4
+
+*/
